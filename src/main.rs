a8931a02cc801171a5eaa7939d04c6e8b945c87d
Akaalroop Singh <spacexplorer11@proton.me>
2025-07-29 16:41:33 +0100

    Added input function
    Signed-off-by: Akaalroop Singh <spacexplorer11@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use rustyline::DefaultEditor;
+use rustyline::error::ReadlineError;
+
+fn input() -> Result<(), ReadlineError> {
+    let mut rl = DefaultEditor::new()?;
+
+    let readline = rl.readline(">> ");
+
+    match readline {
+        Ok(line) => {
+            println!("Line: {}", line);
+        }
+        Err(ReadlineError::Interrupted) => {
+            println!("Interrupted (Ctrl-C)");
+        }
+        Err(ReadlineError::Eof) => {
+            println!("End of File (Ctrl-D)");
+        }
+        Err(err) => {
+            println!("An error occurred: {:?}", err);
+        }
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = input() {
+        eprintln!("Function failed: {:?}", err);
+    }
 }
