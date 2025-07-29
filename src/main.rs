c6946d9ad04ce8d19b69b78e2a08ba9d4c45e738
Akaalroop Singh <spacexplorer11@proton.me>
2025-07-29 18:00:32 +0100

    Added main loop and actual CLI looking stuff
    Signed-off-by: Akaalroop Singh <spacexplorer11@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,58 @@
 use rustyline::DefaultEditor;
 use rustyline::error::ReadlineError;
 
-fn input() -> Result<(), ReadlineError> {
-    let mut rl = DefaultEditor::new()?;
+fn main() {
+    let mut rl = DefaultEditor::new().unwrap();
 
-    let readline = rl.readline(">> ");
+    println!("Welcome to Let's Do It! Please type \"exit\" to exit the program");
 
-    match readline {
-        Ok(line) => {
-            println!("Line: {}", line);
-        }
-        Err(ReadlineError::Interrupted) => {
-            println!("Interrupted (Ctrl-C)");
-        }
-        Err(ReadlineError::Eof) => {
-            println!("End of File (Ctrl-D)");
-        }
-        Err(err) => {
-            println!("An error occurred: {:?}", err);
+    loop {
+        let readline = rl.readline(">> ");
+
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str()).unwrap();
+
+                if !handle_command(&line) {
+                    break;
+                }
+            }
+
+            Err(ReadlineError::Interrupted) => {
+                println!("Interrupted. Type 'exit' or press Ctrl-D to quit.");
+            }
+
+            Err(ReadlineError::Eof) => {
+                println!("Exiting.");
+                break;
+            }
+
+            Err(err) => {
+                println!("An error occurred: {:?}", err);
+                break;
+            }
         }
     }
-
-    Ok(())
 }
 
-fn main() {
-    if let Err(err) = input() {
-        eprintln!("Function failed: {:?}", err);
+/// False to exit, true to keep on going
+fn handle_command(line: &str) -> bool {
+    let parts: Vec<&str> = line.trim().split_whitespace().collect();
+    let command = parts.first().unwrap_or(&"");
+
+    match *command {
+        "hello" => {
+            println!("Hello there!");
+        }
+        "exit" | "quit" | "logout" | "q" => {
+            println!("Exiting & Saving");
+            return false;
+        }
+        "" => {}
+        _ => {
+            println!("Unknown command: '{}'", line);
+        }
     }
+
+    true // Signal to continue the loop.
 }
